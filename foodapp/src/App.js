0b9b2330e04a03906c26e9adb8b03bb3fdc5903d
sandeepurs1 <sandydamn47@gmail.com>
2024-10-25 15:21:58 +0530

    first commit

---
+++ foodapp/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import{BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './Home';
+import Order from './Order';
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path ="/" element={<Home/>}/>
+        <Route path = "/Orders" element={<Order/>}/>
+
+        
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
